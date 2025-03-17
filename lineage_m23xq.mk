f0cc02555fc47b8c8d9ba9e7b9c68f4c002d41c6
Aflaungos <vimavick@gmail.com>
2025-03-17 10:24:03 -0300

    m23xq: Remove Emulated Storage

--- lineage_m23xq.mk
+++ lineage_m23xq.mk
@@ -26,9 +26,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit Emulated Storage
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # Dalvik Heap
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
